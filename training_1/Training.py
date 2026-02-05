c338ad54b224285b2d16ece427f655f2854c233b
Piyush Kumar <piyushkumarsjs@gmail.com>
2026-02-05 21:49:33 +0530

    Add files via upload

---
+++ training_1/Training.py
@@ -0,0 +1,91 @@
+                #  for loop
+# for i in range(5):
+#     print(i)  
+
+                #  while loop
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
+
+                #  for loop
+# for i in range(1, 11):
+#     print(i)       
+
+                #  while loop  
+# count = 1
+# sum = 0
+# while count <= 10:
+#     sum += count
+#     count += 1   
+# print("The sum of the first 10 natural numbers is:", sum)                    
+
+                #   for loop
+# num = int(input("Enter a number: "))
+# multiplication_table = []
+# for i in range(1, 11):
+#     multiplication_table.append(num * i)
+# print("Multiplication Table of", num)
+# for i in range(1, 11):
+#     print(num, "x", i, "=", multiplication_table[i-1])
+
+
+                #   while loop
+# num = int(input("Enter a number: "))
+# factorial = 1
+# count = 1
+# while count <= num:
+#     factorial *= count
+#     count += 1
+# print("Factorial of", num, "is:", factorial)
+
+            #    while loop
+# num = int(input("Enter a number: "))
+# reverse = 0
+# while num > 0:
+#     digit = num % 10
+#     reverse = reverse * 10 + digit
+#     num //= 10
+# print("Reversed Number is:", reverse)
+
+                #   for loop
+                
+# num = int(input("Enter a number: "))
+# for x in range(2, num + 1):
+#     if x % 2 == 0:
+#         print(x)
+
+
+                #   while loop
+# num = int(input("Enter a number: "))    
+# sum = 0
+# rem=0
+# while num>0:
+#     rem=num%10
+#     sum = sum + rem
+#     num= num//10
+# print("The sum of the digits is:", sum)
+
+
+# for i in range(1,5):
+#     print('*' * i)
+
+# for i in range(1,7):
+#      for j in range(1,6):
+#          print(f"{j}" if j<i else " ", end="")
+#      print()
+
+
+# for i in range(5,0,-1):
+#     print( '*' * i)
+
+
+# for i in range(0,7):
+#     for j in range(0,6):
+#         print(f"{i}" if j<i else " ", end="")
+#     print()
+
+
+
+
+
